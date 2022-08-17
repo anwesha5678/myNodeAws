dd3cefce89268da9fb7e1aeb637a34cf5d7acc67
anwesha123 <anwesha@wrctpl.com>
2022-08-18 02:32:48 +0530

    added chnages

--- database.js
+++ database.js
@@ -1,5 +1,34 @@
-const databaseData = {
-    //MYSQL_USER: 'root' MYSQL_PASSWORD: 'MyNewPass1!' MYSQL_DATABASE: 'lotr' MYSQL_HOST: '43.205.111.179'
+const mysql = require('mysql2')
+
+require('dotenv').config()
+
+
+const pool = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE
+}).promise()
+
+module.export =  async function getNotes() {
+  const [rows] = await pool.query("SELECT * FROM notes")
+  return rows
 }
 
-module.exports = databaseData
+module.export =  async function getNote(id) {
+  const [rows] = await pool.query(`
+  SELECT * 
+  FROM notes
+  WHERE id = ?
+  `, [id])
+  return rows[0]
+}
+
+module.export =  async function createNote(title, contents) {
+  const [result] = await pool.query(`
+  INSERT INTO notes (title, contents)
+  VALUES (?, ?)
+  `, [title, contents])
+  const id = result.insertId
+  return getNote(id)
+}
--- index.js
+++ index.js
@@ -4,16 +4,32 @@ require('dotenv').config()
 let port = process.env.PORT
 //const database = require('./database.js')
 const mysql = require('mysql2');
+const { getNotes, getNote, createNote } = require('./database.js')
+// const pool = mysql.createPool({
+//   user: process.env.MYSQL_USER,
+//   password: process.env.MYSQL_PASSWORD,
+//   database: process.env.MYSQL_DATABASE,
+//   host: process.env.MYSQL_HOST
 
-const pool = mysql.createPool({
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-  host: process.env.MYSQL_HOST
+// })
 
+app.use(express.json())
+app.get("/notes", async (req, res) => {
+  const notes = await getNotes()
+  res.send(notes)
 })
 
+app.get("/notes/:id", async (req, res) => {
+  const id = req.params.id
+  const note = await getNote(id)
+  res.send(note)
+})
 
+app.post("/notes", async (req, res) => {
+  const { title, contents } = req.body
+  const note = await createNote(title, contents)
+  res.status(201).send(note)
+})
 
 
 
